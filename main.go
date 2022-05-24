f55a6809bb6e36f48cbc4300c8f151e434e97cab
Aleksandrs Antonovs <aleksandrs.antonovs@roycetechnology.com>
2022-05-24 11:55:54 +0200

    Fix

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ const (
 	bytesToWrite    int64 = 100 * 1024 * 1024 * 1024 // 100Gb
 	maxBytesPerFile       = 512 * 1024 * 1024        // 512Mb
 	outputDir             = "./data"
+
+	sectorSize = 1024
 )
 
 func main() {
@@ -21,14 +23,17 @@ func main() {
 	// Create output directory.
 	os.Mkdir(outputDir, 0777)
 
-	var currentFileSize int64
+	var currentFileSize, writtenSum int64
 	var currentFile *os.File
 
 	startTime := time.Now()
 
 	// Write random bytes.
-	for i := int64(0); i < bytesToWrite; i++ {
-		randByte := byte(randomInt(0, 256))
+	for {
+		randBytes, randBytesErr := randomBytes(sectorSize)
+		if randBytesErr != nil {
+			log.Fatalf("failed to generate random bytes: %v", randBytesErr)
+		}
 
 		if currentFile == nil {
 			fName, _ := randomHex(10)
@@ -40,30 +45,43 @@ func main() {
 			}
 		}
 
-		if _, err := currentFile.Write([]byte{randByte}); err != nil {
+		if _, err := currentFile.Write(randBytes); err != nil {
 			log.Fatalf("failed to write into file: %v", err)
 		}
 
-		currentFileSize++
+		currentFileSize += sectorSize
+		writtenSum += sectorSize
 
 		if currentFileSize >= maxBytesPerFile {
 			currentFile.Close()
 			currentFile = nil
 			currentFileSize = 0
 		}
+
+		if writtenSum >= bytesToWrite {
+			if currentFile != nil {
+				currentFile.Close()
+			}
+			break
+		}
 	}
 
-	fmt.Printf("process finished in %d seconds", int(time.Since(startTime).Seconds()))
-}
-
-func randomInt(min, max int) int {
-	return rand.Intn(max-min) + min
+	fmt.Printf("process finished in %d seconds\n", int(time.Since(startTime).Seconds()))
 }
 
 func randomHex(n int) (string, error) {
-	bytes := make([]byte, n)
-	if _, err := rand.Read(bytes); err != nil {
+	bytes, err := randomBytes(n)
+	if err != nil {
 		return "", err
 	}
 	return hex.EncodeToString(bytes), nil
+}
+
+func randomBytes(n int) ([]byte, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
 }
